6051d2fe479d3749910f6e62dc1d151fd4a41323
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2023-07-13 22:03:32 +0200

    transition between pages implemented

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,11 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.driver.find_element('css selector', "#login_link")
+        login_link = self.driver.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present('css selector', "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,19 +1,22 @@
 import pytest
-from selenium.webdriver.common.by import By
-import time
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(driver, url=link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(driver, driver.current_url)
+    login_page.should_be_login_page()
 
 
+@pytest.mark.skip
 def test_guest_should_see_login_link(driver):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(driver, url=link)
     page.open()
     page.should_be_login_link()
+
