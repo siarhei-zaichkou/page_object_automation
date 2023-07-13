6cfdef6074c85abf2640c98782bf9d6ee75384e5
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2023-07-13 19:10:21 +0200

    add login test in page object style

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,10 @@
 from selenium.webdriver.common.by import By
 import time
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
-    driver.get(link)
-    go_to_login_page(driver)
-
-
-def go_to_login_page(driver):
-    login_link = driver.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(driver, url=link)
+    page.open()
+    page.go_to_login_page()
