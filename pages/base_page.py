0ce4d747c1e4a32c4c4a67591f02fe8e04f41c86
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2023-07-13 19:45:39 +0200

    add is element present checker

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
+
 
 class BasePage:
-    def __init__(self, driver, url):
+    def __init__(self, driver, url, timeout=5):
         self.driver = driver
         self.url = url
+        self.driver.implicitly_wait(timeout)
 
     def open(self):
         self.driver.get(self.url)
+
+    def is_element_present(self, by, value):
+        try:
+            self.driver.find_element(by, value)
+        except NoSuchElementException:
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,19 @@
+import pytest
 from selenium.webdriver.common.by import By
 import time
 from .pages.main_page import MainPage
 
 
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(driver, url=link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(driver):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(driver, url=link)
+    page.open()
+    page.should_be_login_link()
